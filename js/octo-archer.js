a9582d4849d31426cb3e3a6e4b1935afdd1a57b0
jamiep <sysadmin8@ICTOFF-004.lordwandsworth.org>
2015-05-22 13:22:59 +0100

    Proper Fix for Window Resizing issue
    Issue fixed for keeping one page in the center of the window on resize
    and adjusting the position[] to compensate for the changed scale. Plus a
    catch to stop the event running to often and causing lag.

--- js/octo-archer.js
+++ js/octo-archer.js
@@ -1,61 +1,38 @@
 var position = [];
 var curposition = 1;
 var toppercentage
-var pagetotal = 0;
+var pagetotal = 1;
 
 $(document).ready(function() {
 			$(".container").css("height", $(window).innerHeight());
 			setpages();
 			createnav();
 			createbases();
-			var resizeTimer = false;
+			var resizeTimer;
 			$(window).on('resize', function(e) {
-					switch(curposition){
-						case 1:
-						$(".container").css("height", $(window).innerHeight());
-						setpages();
-						break;
-						case 2:
-						$(".container").css("height", $(window).innerHeight());
-						var hig = $(".container").css("height");
-						console.log(hig);
-						$("#wrap").css("top", -(parseInt(hig)) + "px");
-						break;
-						case 3:
-						$(".container").css("height", $(window).innerHeight());
-						var hig = $(".container").css("height");
-						console.log(hig);
-						$("#wrap").css("top", -(parseInt(hig)*2) + "px");
-						break;					
-						case 4:
-						$(".container").css("height", $(window).innerHeight());
-						var hig = $(".container").css("height");
-						console.log(hig);
-						$("#wrap").css("top", -(parseInt(hig)*3) + "px");
-						break;
-					}
+					clearTimeout(resizeTimer);
+					resizeTimer = setTimeout(function(){						
+							if(curposition == 1 || curposition == pagetotal+1){
+							$(".container").css("height", $(window).innerHeight());
+							$("#wrap").css("top", 0);
+							var hig = $(".container").css("height");
+							
+								for(i = 0; i< position.length; i++){
+									position[i] = ((parseInt(hig))*i);
+								}
+							
+							}else{
+								
+							$(".container").css("height", $(window).innerHeight());
+							var hig = $(".container").css("height");
+							$("#wrap").css("top", -(parseInt(hig)*(curposition-1)) + "px");
+							
+								for(i = 0; i< position.length; i++){
+									position[i] = ((parseInt(hig))*i);
+								}
+						}						
+					}, 250);
 					
-					
-					/*var oldheight = parseInt($(".container").css("height"));
-					var newheight = $(window).innerHeight();
-					var difference = oldheight - newheight;
-					console.log("oldheight: "+oldheight + "- newheight" + newheight + "= differenece" + difference);
-					$(".container").css("height", $(window).innerHeight());
-					var curtop = parseInt($("#wrap").css("top"));
-					console.log(curposition);
-					if(curposition != 0 && curposition != 1){
-						$("#wrap").css("top", (curtop - (difference))+"px");
-						
-					} else {
-						setpages();
-					}
-					
-					
-					/*var curpos = $("#wrap").css("top");
-					$(".container").css("height", $(window).innerHeight());
-					$("#wrap").css("top", 0);
-					$("#activeball").css("top", 0);
-					setpages();*/
 			});
 });
 function setpages(){
